40d4fb63b8d972c89cb9af2a7f1457f5c3d97b7f
Adrian Chan <adrianchanuk@gmail.com>
2020-12-03 20:25:22 +0800

    changes

---
+++ src/Pages/BrandAccess/BrandAccess.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BrandAccess.style.scss'
+
+function BrandAccess(){
+    return (
+    <div class="BrandAccessCard">
+        <h2>Brand Access</h2>
+    </div>
+    )
+}
+
+
+export default BrandAccess;
---
+++ src/Pages/UserAccess/UserAccess.components.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./UserAccess.style.scss"
+
+
+function UserAccess(){
+    return (
+    <div class="UserAccessCard">
+        <h2>User Access</h2> 
+    </div>
+    )
+}
+
+
+export default UserAccess;
